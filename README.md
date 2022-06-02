24d504b1bbd9789e153b3b60776afd0807ba05b0
vie10 <hrekhviktor@gmail.com>
2022-06-02 20:38:09 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+![Banner](.github/banner.png)
+
+# PaperKit — framework that helps you develop Paper plugins fast and easy.
+
+![Version badge](https://img.shields.io/static/v1?label=Version&message=1.0-dev&color=red&style=for-the-badge)
+
+Framework based on Paper api to build awesome plugins easy to extend and support.
+Turns plugins development to convenient and fast process.
+Currently, is under active development process, so isn't recommended to use in production yet.
+
+### Advantages of using PaperKit:
+
+- Fully support of coroutines
+- Easy-To-Extend structure
+- Fast development
+- Convenient config system
+- High-customisable
+- Easy to interact between plugins
+
+### Possible negatives:
+
+- Native support only for latest versions.
---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+///////////////////////////////////////////////////////////////////////////
+// Versions
+///////////////////////////////////////////////////////////////////////////
+
+val paper = "1.18.2-R0.1-SNAPSHOT"
+val coroutines = "1.6.2"
+val hoplite = "2.1.5"
+val kotlinxSerialization = "1.3.3"
+val koin = "3.1.6"
+val ronfy = "1.1"
+val ktorm = "3.4.1"
+
+///////////////////////////////////////////////////////////////////////////
+// Settings
+///////////////////////////////////////////////////////////////////////////
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jetbrains.dokka") version "1.6.21"
+}
+
+group = "online.viestudio"
+version = "1.0-dev"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    // Kotlin
+    api("org.jetbrains.kotlinx", "kotlinx-coroutines-core", coroutines)
+    // Configuration
+    api("com.sksamuel.hoplite", "hoplite-yaml", hoplite)
+    api("com.github.vie10", "ronfy", ronfy)
+    // Paper
+    api("io.papermc.paper", "paper-api", paper)
+    // Json
+    api("org.jetbrains.kotlinx", "kotlinx-serialization-json", kotlinxSerialization)
+    // Dependency injection
+    api("io.insert-koin", "koin-core", koin)
+    // Database
+    api("org.ktorm", "ktorm-core", ktorm)
+}
+
+publishing {
+    repositories {
+        maven("https://maven.pkg.github.com/paper-kit/paper-kit") {
+            name = "GitHubPackages"
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Tasks
+///////////////////////////////////////////////////////////////////////////
+
+with(tasks) {
+    processResources {
+        val properties = linkedMapOf(
+            "version" to version.toString(),
+        )
+
+        filesMatching(
+            setOf("plugin.yml")
+        ) {
+            expand(properties)
+        }
+    }
+
+    shadowJar {
+        archiveFileName.set("${project.name}-${project.version}.jar")
+
+        fun ResolvedDependency.containsOrParents(content: String): Boolean {
+            return name.contains(content) || parents.find { it.containsOrParents(content) } != null
+        }
+
+        dependencies {
+            exclude {
+                it.containsOrParents("paper-api")
+            }
+        }
+    }
+
+    wrapper {
+        version = "7.4.2"
+    }
+
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+}

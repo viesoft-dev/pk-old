9a4f9a7326c04ef429f93cb0e7b09d321b81141a
Viktor Hrekh <hrekhviktor@gmail.com>
2022-06-07 14:02:36 +0300

    Migrate blocking to launch in components

--- src/main/kotlin/online/viestudio/paperkit/command/KitCommandExecutor.kt
+++ src/main/kotlin/online/viestudio/paperkit/command/KitCommandExecutor.kt
@@ -1,16 +1,21 @@
 package online.viestudio.paperkit.command
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+import online.viestudio.paperkit.koin.plugin
+import online.viestudio.paperkit.plugin.KitPlugin
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.koin.core.component.KoinComponent
 
 class KitCommandExecutor(
     private val command: KitCommand,
-) : CommandExecutor {
+) : CommandExecutor, KoinComponent {
+
+    private val plugin: KitPlugin by plugin()
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        runBlocking { this@KitCommandExecutor.command.execute(sender, args) }
+        plugin.serverScope.launch { this@KitCommandExecutor.command.execute(sender, args) }
         return true
     }
 }
--- src/main/kotlin/online/viestudio/paperkit/command/KitCommandTabCompleter.kt
+++ src/main/kotlin/online/viestudio/paperkit/command/KitCommandTabCompleter.kt
@@ -1,13 +1,18 @@
 package online.viestudio.paperkit.command
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+import online.viestudio.paperkit.koin.plugin
+import online.viestudio.paperkit.plugin.KitPlugin
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
+import org.koin.core.component.KoinComponent
 
 class KitCommandTabCompleter(
     private val kitCommand: KitCommand,
-) : TabCompleter {
+) : TabCompleter, KoinComponent {
+
+    private val plugin: KitPlugin by plugin()
 
     override fun onTabComplete(
         sender: CommandSender,
@@ -16,10 +21,12 @@ class KitCommandTabCompleter(
         args: Array<String>,
     ): MutableList<String> {
         val currentArgument = args.lastOrNull() ?: return mutableListOf()
-        return runBlocking {
-            kitCommand.complete(sender, args).filter {
+        var result = emptyList<String>()
+        plugin.serverScope.launch {
+            result = kitCommand.complete(sender, args).filter {
                 it.startsWith(currentArgument, true)
-            }.toMutableList()
+            }
         }
+        return result.toMutableList()
     }
 }

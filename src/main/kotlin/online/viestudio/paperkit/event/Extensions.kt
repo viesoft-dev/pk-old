bb85eae8db04dbf56bf21d3836580eecdce3c78c
Viktor Hrekh <hrekhviktor@gmail.com>
2022-08-11 18:37:20 +0300

    Event util extensions

---
+++ src/main/kotlin/online/viestudio/paperkit/event/Extensions.kt
@@ -0,0 +1,6 @@
+package online.viestudio.paperkit.event
+
+import online.viestudio.paperkit.plugin.KitPlugin
+import org.bukkit.event.Event
+
+fun KitPlugin.callEvent(event: Event) = server.pluginManager.callEvent(event)

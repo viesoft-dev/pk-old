46b1d269f1cdf0285e5b52841d7a6f546c459f06
Viktor Hrekh <hrekhviktor@gmail.com>
2022-08-16 10:18:49 +0300

    Online player validator

--- src/main/kotlin/online/viestudio/paperkit/command/argument/Validators.kt
+++ src/main/kotlin/online/viestudio/paperkit/command/argument/Validators.kt
@@ -2,8 +2,25 @@
 
 package online.viestudio.paperkit.command.argument
 
+import org.bukkit.Server
 import kotlin.reflect.KClass
 import kotlin.reflect.full.staticFunctions
+
+inline fun Argument.Builder.onlinePlayerValidator(
+    server: Server,
+    excludeSender: Boolean = true,
+    crossinline onNotOnline: suspend () -> String,
+) {
+    validator { sender, _, input ->
+        val onlinePlayers = if (excludeSender) {
+            server.onlinePlayers.filter { it.name != sender.name }
+        } else server.onlinePlayers
+        val player = onlinePlayers.find { it.name == input }
+        if (player == null) {
+            onNotOnline()
+        } else null
+    }
+}
 
 inline fun Argument.Builder.intValidator(crossinline onNotInt: suspend () -> String) {
     validator { _, _, input ->

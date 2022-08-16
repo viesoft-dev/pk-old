20017d5b843f44285c69e5c985a546f0ee70c944
Viktor Hrekh <hrekhviktor@gmail.com>
2022-08-16 10:11:12 +0300

    Add default online players completer

--- src/main/kotlin/online/viestudio/paperkit/command/argument/Completes.kt
+++ src/main/kotlin/online/viestudio/paperkit/command/argument/Completes.kt
@@ -2,8 +2,18 @@
 
 package online.viestudio.paperkit.command.argument
 
+import org.bukkit.Server
 import kotlin.reflect.KClass
 import kotlin.reflect.full.staticFunctions
+
+fun Argument.Builder.onlinePlayersCompleter(server: Server, excludeSender: Boolean = true) {
+    completer { sender, _, _ ->
+        val nicknames = server.onlinePlayers.map { it.name }
+        if (excludeSender) {
+            nicknames.filter { it != sender.name }
+        } else nicknames
+    }
+}
 
 inline fun Argument.Builder.emptyCompleter(crossinline block: suspend () -> List<String>) {
     completer { _, _, _ -> block() }
